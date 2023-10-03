736d6819b5754738876b483864d317d0908ed836
aCloud1 <almondCloud@outlook.com>
2023-10-03 19:51:05 +0300

    User is authorized after successful registration.

--- Services/UserSession.cs
+++ Services/UserSession.cs
@@ -4,5 +4,13 @@
     {
         public string email_address { get; set; }
         public string role { get; set; }
+
+        public UserSession() { }
+
+        public UserSession(string email_address, string role)
+        {
+            this.email_address = email_address;
+            this.role = role;
+        }
     }
 }
