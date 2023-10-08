450e08bad65b08d1169d45652779ce3ec3d36c10
aCloud1 <almondCloud@outlook.com>
2023-10-08 15:18:58 +0300

    * various fixes * EmployeeActions interface

--- Services/Account.cs
+++ Services/Account.cs
@@ -26,11 +26,6 @@ namespace OnlineStore.Services
     }
 
 
-
-    // todo: differentiante between these based on the domain of the email.
-
-
-    // 
     public class Customer : Account
     {
         public Customer(string id, string email_address, string password, Person personal_data)
@@ -40,64 +35,21 @@ namespace OnlineStore.Services
 
     public interface EmployeeActions
     {
-        public void ajustAvailableItemCount(int count);
+        public void clearTransactions();
     }
 
-    // If a user is logging in with company's email, he is
     public class Employee : Account, EmployeeActions
     {
         public Employee(string id, string email_address, string password, Person personal_data)
             : base(id, ROLES.Admin, email_address, password, personal_data) { }
 
-        public void ajustAvailableItemCount(int count)
+
+        public void clearTransactions()
         {
-            throw new NotImplementedException();
+            this.transactions.Clear();
         }
     }
 
 
-    public class Person : IFormattable
-    {
-        public string first_name;
-        public string second_name;
-        public string home_address;
-        public string phone_number;
 
-        public Person() {}
-
-        public Person(string first_name, string second_name, string home_address, string phone_number)
-        {
-            this.first_name = first_name;
-            this.second_name = second_name;
-            this.home_address = home_address;
-            this.phone_number = phone_number;
-		}
-
-		public string ToString(string? format, IFormatProvider? formatProvider)
-		{
-			string buffer = "";
-            if (format != null)
-            {
-                switch (format.ToLower())
-                {
-                    case "text":
-                        buffer = $"{first_name} {second_name} lives at {home_address} and is reachable by calling {phone_number}";
-                        break;
-
-                    case "json":
-                        buffer += "{\n";
-                        buffer += $"  \"first_name\": \"{first_name}\",\n";
-                        buffer += $"  \"second_name\": \"{second_name}\",\n";
-                        buffer += $"  \"home_address\": \"{home_address}\",\n";
-                        buffer += $"  \"phone_number\": \"{phone_number}\"\n";
-                        buffer += "}";
-                        break;
-
-                    default:
-                        break;
-                }
-            }
-            return buffer;
-		}
-	}
 }
