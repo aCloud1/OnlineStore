cf68a2a5502208c3f09bca6228bfc414bd43012d
aCloud1 <almondCloud@outlook.com>
2023-10-08 12:19:28 +0300

    Checkout functionality.

---
+++ Services/CheckoutService.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineStore.Services
+{
+	public static class CheckoutService
+	{
+		public static int calculateTotal(List<Item> items)
+		{
+			return items.Sum(x => x.price);
+		}
+	}
+}
