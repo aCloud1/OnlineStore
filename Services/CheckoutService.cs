6c11de8b1571a198d48402ff9f08a1047d4a6d03
aCloud1 <almondCloud@outlook.com>
2023-10-08 13:24:21 +0300

    Move Transactions to a separate file.

--- Services/CheckoutService.cs
+++ Services/CheckoutService.cs
@@ -12,27 +12,4 @@
 			return new Transaction(account_id, total);
 		}
 	}
-
-
-	public class Transaction
-	{
-		public string id;
-		public string account_id;
-		public int total;
-		public DateOnly date;
-
-
-		public Transaction()
-		{
-			this.id = Id.generate();
-		}
-
-		public Transaction(string account_id, int total)
-		{
-			this.id = Id.generate();
-			this.account_id = account_id;
-			this.total = total;
-			this.date = DateOnly.FromDateTime(DateTime.UtcNow);
-		}
-	}
 }
---
+++ Services/Transaction.cs
@@ -0,0 +1,24 @@
+﻿namespace OnlineStore.Services
+{
+	public class Transaction
+	{
+		public string id;
+		public string account_id;
+		public int total;
+		public DateOnly date;
+
+
+		public Transaction()
+		{
+			this.id = Id.generate();
+		}
+
+		public Transaction(string account_id, int total)
+		{
+			this.id = Id.generate();
+			this.account_id = account_id;
+			this.total = total;
+			this.date = DateOnly.FromDateTime(DateTime.UtcNow);
+		}
+	}
+}
