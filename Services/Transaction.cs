5f16524359b44ae79e84602b02064648b25a834c
aCloud1 <almondCloud@outlook.com>
2023-10-08 14:22:51 +0300

    Transactions can now be sorted (IComparable, IEquatable)

--- Services/Transaction.cs
+++ Services/Transaction.cs
@@ -1,6 +1,8 @@
-﻿namespace OnlineStore.Services
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace OnlineStore.Services
 {
-	public class Transaction
+	public class Transaction : IComparable<Transaction>, IEquatable<Transaction>
 	{
 		public string id;
 		public string account_id;
@@ -19,6 +21,24 @@
 			this.account_id = account_id;
 			this.total = total;
 			this.date = DateOnly.FromDateTime(DateTime.UtcNow);
+		}
+
+		public int CompareTo(Transaction? other)
+		{
+			if(other == null)
+				return 1;
+
+			int result = date.CompareTo(other.date);
+			if(result == 0)
+				result = total.CompareTo(other.total);
+			return result;
+		}
+
+		public bool Equals(Transaction? other)
+		{
+			return date.Equals(other) &&
+					 id.Equals(other.id) &&
+				  total.Equals(other.total);
 		}
 	}
 }
