4c1802840e027346f2c0d8ceb1e660424ddfb7ca
aCloud1 <almondCloud@outlook.com>
2023-10-12 09:35:44 +0300

    Removed comments

--- Services/Shop.cs
+++ Services/Shop.cs
@@ -18,34 +18,5 @@
         {
             return stock_manager.getItemById(itemId);
         }
-
-        /*
-        public List<Item> filter(Predicate<> pr, ItemFieldNames field)
-        {
-            List<Item> results = new List<Item>();
-            switch(field)
-            {
-				case ItemFieldNames.Id:
-					stock_manager.items.FindAll(item => pr(item.id));
-					break;
-
-				case ItemFieldNames.Name:
-					stock_manager.items.FindAll(item => pr(item.name));
-					break;
-
-                case ItemFieldNames.Price:
-					stock_manager.items.FindAll(item => pr(item.price));
-                    break;
-
-				case ItemFieldNames.Category:
-					stock_manager.items.FindAll(item => pr(item.category));
-                    break;
-
-				default:
-                    break;
-            }
-            return results;
-        }
-        */
     }
 }
